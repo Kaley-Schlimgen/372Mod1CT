25ee4392fbf0f0078ed0f438c2412c6a9564181b
Kaley-Schlimgen <kaleynicole89@gmail.com>
2024-11-23 10:02:32 -0700

    fist commit

---
+++ 372M1CT_test/src/main/java/com/mycompany/m1ct_test/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.m1ct_test;
+
+/**
+ *
+ * @author kaleyschlimgen
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
